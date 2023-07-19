289ccf1b8ddbb324d62789b7c227dfda526e6692
Taylor Brown <taylor@Taylors-MacBook-Pro.local>
2023-07-19 17:20:36 -0600

    Added all of JS

--- Assets/Passcode.js
+++ Assets/Passcode.js
@@ -1,31 +1,81 @@
-// JavaScript code (Help from some Tutorials/Classmates/Instructor)
-var letters = ('abcdefghijklmnopqrstuvwxyz');
-var numbers = ('1234567890');
-var special = ('!@#$%^&*+=?/');
+// Assignment Code
+var letters = 'abcdefghijklmnopqrstuvwxyz';
+var capLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numbers = '1234567890';
+var special = '!@#$%^&*+=?/';
 
-var generatePassword = function() {
-    var letteroption = window.confirm('Include letters?');
-    var numberoption = window.confirm('Include numbers?');
-    var specialoption = window.confirm('Include special characters?');
+var generateBtn = document.querySelector("#generate");
+
+var generatePassword = function () {
     var lengthoption = window.prompt('Enter desired length of your password, between 8 to 128');
+
+    if (lengthoption < 8) {
+        alert('asjdkasdjnasd')
+        return null
+    }
+    if (lengthoption > 128) {
+        alert('assdssdfj')
+        return null
+    }
+    // Number.isNaN
+    if (Number.isNaN(lengthoption)){
+        alert('must be a number')
+        return null
+    }
+
+    var lettersoption = window.confirm('Include letters?');
+    var numbersoption = window.confirm('Include numbers?');
+    var specialoption = window.confirm('Include special characters?');
+    var capLettersoption = window.confirm('Include capital letters?');
+
+    // WHEN I answer each prompt
+    // THEN my input should be validated and at least one character type should be selected
 
     var specialcharacters = []
 
-    if (letteroption) {
-        var index = Math.floor(Math.random() * letters.length);
-        console.log(letters[index]);
-        specialcharacters.push(letters[index]);
+    if (lettersoption) {
+        specialcharacters = specialcharacters.concat(letters.split(''));
+        console.log(specialcharacters);
     }
 
-    if (numberoption) {
-        var index = Math.floor(Math.random() * numbers.length);
-        console.log(numbers[index]);
-        specialcharacters.push(numbers[index]);    
+
+    if (numbersoption) {
+        specialcharacters =specialcharacters.concat(numbers.split(''));
+        console.log(specialcharacters);
+    }
+
+    if (specialoption) {
+
+        specialcharacters =specialcharacters.concat(special.split(''));
+        console.log(specialcharacters);
+    }
+
+    if (capLettersoption) {
+        specialcharacters =specialcharacters.concat(capLetters.split('')); console.log(specialcharacters);
+    }
+
+    var temp = []
+
+    for (var i = 0; i < lengthoption; i++) {
+        temp.push(specialcharacters[Math.floor(Math.random() * specialcharacters.length)]);
     }
     
-    if (specialoption) {
-        var index = Math.floor(Math.random() * special.length);
-        console.log(special[index]);
-        specialcharacters.push(special[index]);    
-    }
+    var specialcharacters = temp.join('');
+    console.log(temp.join(''))
+    // how do we convert an array into a string with no commas?
+    
+    return specialcharacters
 }
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
