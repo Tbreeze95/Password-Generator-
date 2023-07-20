758afa8de44f263e86a35aae0a8b4bc82ecf06d5
Taylor Brown <taylor@Taylors-MacBook-Pro.local>
2023-07-20 12:04:56 -0600

    Added comments and cleaned up the code

--- Assets/Passcode.js
+++ Assets/Passcode.js
@@ -3,21 +3,23 @@ var letters = 'abcdefghijklmnopqrstuvwxyz';
 var capLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var numbers = '1234567890';
 var special = '!@#$%^&*+=?/';
-
 var generateBtn = document.querySelector("#generate");
 
+// Created a function to prompt the user to select their desired character length
 var generatePassword = function () {
     var lengthoption = window.prompt('Enter desired length of your password, between 8 to 128');
 
+    // Provided some if statemetnts to hep alleviate user errors and redirect them back to a successful point 
+
     if (lengthoption < 8) {
-        alert('asjdkasdjnasd')
+        alert('Character length must be greater than or equal to 8 characters')
         return null
     }
     if (lengthoption > 128) {
-        alert('assdssdfj')
+        alert('Character length must be less than or equal to 128 characters')
         return null
     }
-    // Number.isNaN
+    // Number.isNaN was used here to make sure the user submits a number and not other "invalid" characters.. if they do submit an invalid input the password won't create and they'll be redirected back to the starting point 
     if (Number.isNaN(lengthoption)){
         alert('must be a number')
         return null
@@ -28,11 +30,9 @@ var generatePassword = function () {
     var specialoption = window.confirm('Include special characters?');
     var capLettersoption = window.confirm('Include capital letters?');
 
-    // WHEN I answer each prompt
-    // THEN my input should be validated and at least one character type should be selected
-
     var specialcharacters = []
 
+    // used concat and split on this if statements to help combine arrays into a randomized string (Hence how the passcode comes along)
     if (lettersoption) {
         specialcharacters = specialcharacters.concat(letters.split(''));
         console.log(specialcharacters);
@@ -55,7 +55,7 @@ var generatePassword = function () {
     }
 
     var temp = []
-
+    // 
     for (var i = 0; i < lengthoption; i++) {
         temp.push(specialcharacters[Math.floor(Math.random() * specialcharacters.length)]);
     }
